4f8ec7c7041262a6191512ab141b99f6006440c5
Lova Widmark <znurree@gmail.com>
2018-11-29 18:04:26 +0100

    Experimenting with sf::Drawable

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,76 @@
 #include <SFML/Graphics.hpp>
 
+template<typename T>
+void UNUSED(T &)
+{
+}
+
+class Player
+{
+	public:
+		Player()
+			: m_x(0)
+			, m_y(0)
+		{
+		}
+
+		int x() const
+		{
+			return m_x;
+		}
+
+		void setX(int x)
+		{
+			m_x = x;
+		}
+
+		int y()
+		{
+			return m_y;
+		}
+
+		void setY(int y)
+		{
+			m_y = y;
+		}
+
+	private:
+		int m_x;
+		int m_y;
+};
+
+class PlayerDrawable : public sf::Drawable
+{
+	public:
+		PlayerDrawable(Player &player)
+			: m_player(player)
+		{
+			m_texture.loadFromFile("resources/player.png");
+		}
+
+	protected:
+		void draw(sf::RenderTarget &target, sf::RenderStates states) const override
+		{
+			UNUSED(states);
+
+			sf::Sprite sprite(m_texture);
+			sprite.setPosition(m_player.x(), m_player.y());
+
+			target.draw(sprite);
+		}
+
+	private:
+		sf::Texture m_texture;
+
+		Player &m_player;
+};
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "ld43");
+
+	Player player;
+	PlayerDrawable playerDrawable(player);
 
 	while (window.isOpen())
 	{
@@ -10,6 +78,27 @@ int main()
 
 		while (window.pollEvent(event))
 		{
+			if (event.type == sf::Event::KeyPressed)
+			{
+				switch (event.key.code)
+				{
+					case sf::Keyboard::W:
+						player.setY(player.y() - 32);
+						break;
+					case sf::Keyboard::A:
+						player.setX(player.x() - 32);
+						break;
+					case sf::Keyboard::S:
+						player.setY(player.y() + 32);
+						break;
+					case sf::Keyboard::D:
+						player.setX(player.x() + 32);
+						break;
+					default:
+						break;
+				}
+			}
+
 			if (event.type == sf::Event::Closed)
 			{
 				window.close();
@@ -17,6 +106,7 @@ int main()
 		}
 
 		window.clear(sf::Color::Black);
+		window.draw(playerDrawable);
 		window.display();
 	}
 
