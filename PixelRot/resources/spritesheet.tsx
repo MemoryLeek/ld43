b0154e21c93c8890d95b762957611c6939ce2b16
Lova Widmark <znurree@gmail.com>
2018-12-03 19:50:11 +0100

    Oops

--- PixelRot/resources/spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="spritesheet" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
- <image source="spritesheet.png" width="2048" height="2048"/>
-</tileset>
